7656d6958c07d12494cd0ee3a8040b86834df1c5
Tyler Watson <leggettwatson.tyler@gmail.com>
2018-11-26 16:14:33 -0800

    Added Functionality for creating and updating grocery lists using react and ES6

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-// TODO
+class GroceryListItem extends React.Component {
+	constructor(props){
+		super(props);
+ 		this.state = {
+		    done: false,
+		   // bold: false
+		};
+	}
+
+	onListItemClick() {
+	  	this.setState({done: !this.state.done});
+	}
+
+ 	render(){
+		var style = {
+			 textDecoration: this.state.done ? 'line-through' : 'none'
+		};
+		return (
+		    <li style={style} onClick = { this.onListItemClick.bind(this) } key={this.props.key}>{this.props.item}</li>
+		    // <li text={text} onMouse
+		);
+	}
+
+ }
+ const GroceryList = (props) =>{
+	return (
+		<div>
+			<ul>
+				<h2> Grocery List </h2>
+				{props.itemList.map((item ,index) =>{
+					return <GroceryListItem item={item} key={index}/>
+				})}
+			</ul>
+		</div>
+	);
+}
+
+ReactDOM.render(<GroceryList itemList={['Cucumber','Kale','ICE CREAAAMMMM!' ,'apples', 'cheese']}/>, document.getElementById('app'));
